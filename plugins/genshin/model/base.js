d977fccbabaf1d82423e2f47bf0fa3269b19ead7
Kokomi <102026640+yoimiya-kokomi@users.noreply.github.com>
2023-07-04 02:31:22 +0800

    更新3.8相关资源 @Ca(HCO₃)₂

--- plugins/genshin/model/base.js
+++ plugins/genshin/model/base.js
@@ -18,7 +18,7 @@ export default class base {
    * @param pluResPath 插件资源路径
    */
   get screenData() {
-    let headImg = '枫原万叶'
+    let headImg = '优菈'
 
     return {
       saveId: this.userId,
