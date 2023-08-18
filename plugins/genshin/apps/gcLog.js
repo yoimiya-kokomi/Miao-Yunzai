5efa128a621f73631f803ddaa4dff18d88816f8a
bbaban <3102509561@qq.com>
2023-08-18 10:19:48 +0800

    提交星琼、星琼统计

--- plugins/genshin/apps/gcLog.js
+++ plugins/genshin/apps/gcLog.js
@@ -8,7 +8,7 @@ import LogCount from '../model/logCount.js'
 const _path = process.cwd() + '/plugins/genshin'
 
 export class gcLog extends plugin {
-  constructor () {
+  constructor() {
     super({
       name: '抽卡记录',
       dsc: '抽卡记录数据统计',
@@ -55,19 +55,18 @@ export class gcLog extends plugin {
     })
 
     this.androidUrl = 'docs.qq.com/doc/DUWpYaXlvSklmVXlX'
-    this._path = process.cwd().replace(/\\/g, '/')
   }
 
-  async init () {
-    let file = ['./data/gachaJson','./data/srJson','./temp/html/StarRail']
-    for(let i of file){
+  async init() {
+    let file = ['./data/gachaJson', './data/srJson', './temp/html/StarRail']
+    for (let i of file) {
       if (!fs.existsSync(i)) {
         fs.mkdirSync(i)
       }
     }
   }
 
-  accept () {
+  accept() {
     if (this.e.file && this.e.isPrivate) {
       let name = this.e.file?.name
       if (name.includes('txt')) {
@@ -90,7 +89,7 @@ export class gcLog extends plugin {
   }
 
   /** 抽卡记录链接 */
-  async logUrl () {
+  async logUrl() {
     if (!this.e.isPrivate) {
       this.e.reply('请私聊发送链接', false, { at: true })
       return true
@@ -98,13 +97,13 @@ export class gcLog extends plugin {
 
     let data = await new GachaLog(this.e).logUrl()
     if (!data) return
-    let url = this.srHead('gachaLog', data)
-    let img = await puppeteer.screenshot(url, data)
+
+    let img = await puppeteer.screenshot(this.e.isSr ? 'StarRail/gachaLog' : 'gachaLog', data)
     if (img) await this.reply(img)
   }
 
   /** 发送output_log.txt日志文件 */
-  async logFile () {
+  async logFile() {
     if (!this.e.isPrivate) {
       await this.e.reply('请私聊发送日志文件', false, { at: true })
       return true
@@ -121,27 +120,22 @@ export class gcLog extends plugin {
     if (!data) return false
 
     if (typeof data != 'object') return
-    let url='gachaLog'
-    if(this.e.isSr){
-      url ='StarRail/gachaLog'
-      data.tplFile = './plugins/genshin/resources/StarRail/html/gachaLog/gachaLog.html'
-      data.headStyle =  `<style> .head_box { background: url(${this._path}/plugins/genshin/resources/StarRail/img/worldcard/星穹列车.png) #fff;  background-repeat: no-repeat; background-position-x: -10px; background-size: 500px; background-position-y: -90px; }</style>`
-    }
-    let img = await puppeteer.screenshot(url, data)
+
+    let img = await puppeteer.screenshot(this.e.isSr ? 'StarRail/gachaLog' : 'gachaLog', data)
     if (img) await this.reply(img)
   }
 
   /** #抽卡记录 */
-  async getLog () {
+  async getLog() {
     let data = await new GachaLog(this.e).getLogData()
     if (!data) return
-    let url = this.srHead('gachaLog', data)
-    let img = await puppeteer.screenshot(url, data)
+
+    let img = await puppeteer.screenshot(this.e.isSr ? 'StarRail/gachaLog' : 'gachaLog', data)
     if (img) await this.reply(img)
   }
 
   /** 导出记录 */
-  async exportLog () {
+  async exportLog() {
     if (this.e.isGroup) {
       await this.reply('请私聊导出', false, { at: true })
       return
@@ -162,7 +156,7 @@ export class gcLog extends plugin {
     }
   }
 
-  async logXlsx () {
+  async logXlsx() {
     if (!this.e.isPrivate) {
       await this.e.reply('请私聊发送日志文件', false, { at: true })
       return true
@@ -176,7 +170,7 @@ export class gcLog extends plugin {
     await new ExportLog(this.e).logXlsx()
   }
 
-  async logJson () {
+  async logJson() {
     if (!this.e.isPrivate) {
       await this.e.reply('请私聊发送Json文件', false, { at: true })
       return true
@@ -190,11 +184,11 @@ export class gcLog extends plugin {
     await new ExportLog(this.e).logJson()
   }
 
-  async help () {
+  async help() {
     await this.e.reply(segment.image(`file:///${_path}/resources/logHelp/记录帮助.png`))
   }
 
-  async helpPort () {
+  async helpPort() {
     let msg = this.e.msg.replace(/#|帮助/g, '')
 
     if (['电脑', 'pc'].includes(msg)) {
@@ -205,20 +199,12 @@ export class gcLog extends plugin {
       await this.e.reply(segment.image(`file:///${_path}/resources/logHelp/记录帮助-苹果.png`))
     }
   }
-  srHead = (url, data) => {
-    let name = url
-    if (this.e.isSr) {
-      name = `StarRail/${url}`
-      data.tplFile = `./plugins/genshin/resources/StarRail/html/${url}/${url}.html`
-      data.headStyle = `<style> .head_box { background: url(${this._path}/plugins/genshin/resources/StarRail/img/worldcard/星穹列车.png) #fff; background-position-x: -10px; background-repeat: no-repeat; background-size: 540px; background-position-y: -100px; </style>`
-    }
-    return name
-  }
-  async logCount () {
+
+  async logCount() {
     let data = await new LogCount(this.e).count()
     if (!data) return
-    let url = this.srHead('logCount', data)
-    let img = await puppeteer.screenshot(url, data)
+
+    let img = await puppeteer.screenshot(this.e.isSr ? 'StarRail/logCount' : 'logCount', data)
     if (img) await this.reply(img)
   }
 }
--- plugins/genshin/model/base.js
+++ plugins/genshin/model/base.js
@@ -20,6 +20,16 @@ export default class base {
   get screenData() {
     let headImg = '林尼'
 
+    if(this.e?.isSr)
+      return {
+        saveId: this.userId,
+        cwd: this._path,
+        tplFile: `./plugins/genshin/resources/StarRail/html/${this.model}/${this.model}.html`,
+        /** 绝对路径 */
+        pluResPath: `${this._path}/plugins/genshin/resources/StarRail/`,
+        headStyle: `<style> .head_box { background: url(${this._path}/plugins/genshin/resources/StarRail/img/worldcard/星穹列车.png) #fff; background-position-x: -10px; background-repeat: no-repeat; background-size: 540px; background-position-y: -100px; </style>`
+      }
+
     return {
       saveId: this.userId,
       cwd: this._path,
