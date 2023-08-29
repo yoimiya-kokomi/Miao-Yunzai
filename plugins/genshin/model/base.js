4e73dc8f4360da01cb73f212455d18e5f402f271
帮帮 <3102509561@qq.com>
2023-08-29 13:02:57 +0000

    !89 给探索增加更多内容、上一个pr被吞了 * 优化 * 优化 * 优化头像获取 * 七圣 * 修复 * 修复 * 给探索增加更多内容

--- plugins/genshin/model/base.js
+++ plugins/genshin/model/base.js
@@ -1,3 +1,5 @@
+import fs from 'node:fs'
+import _ from 'lodash'
 
 export default class base {
   constructor(e = {}) {
@@ -18,26 +20,27 @@ export default class base {
    * @param pluResPath 插件资源路径
    */
   get screenData() {
-    let headImg = '林尼'
-
-    if (this.e?.isSr)
+    if (this.e?.isSr) {
+      let headImg = _.sample(fs.readdirSync(`${this._path}/plugins/genshin/resources/StarRail/img/worldcard`).filter(file => file.endsWith('.png')))
       return {
         saveId: this.userId,
         cwd: this._path,
         tplFile: `./plugins/genshin/resources/StarRail/html/${this.model}/${this.model}.html`,
         /** 绝对路径 */
         pluResPath: `${this._path}/plugins/genshin/resources/StarRail/`,
-        headStyle: `<style> .head_box { background: url(${this._path}/plugins/genshin/resources/StarRail/img/worldcard/星穹列车.png) #fff; background-position-x: -10px; background-repeat: no-repeat; background-size: 540px; background-position-y: -100px; </style>`,
+        headStyle: `<style> .head_box { background: url(${this._path}/plugins/genshin/resources/StarRail/img/worldcard/${headImg}) #fff; background-position-x: -10px; background-repeat: no-repeat; background-size: 540px; background-position-y: -100px; </style>`,
         srtempFile: 'StarRail/'
       }
-
+    }
+    
+    let headImg = _.sample(fs.readdirSync(`${this._path}/plugins/genshin/resources/img/namecard`).filter(file => file.endsWith('.png')))
     return {
       saveId: this.userId,
       cwd: this._path,
       tplFile: `./plugins/genshin/resources/html/${this.model}/${this.model}.html`,
       /** 绝对路径 */
       pluResPath: `${this._path}/plugins/genshin/resources/`,
-      headStyle: `<style> .head_box { background: url(${this._path}/plugins/genshin/resources/img/namecard/${headImg}.png) #fff; background-position-x: 42px; background-repeat: no-repeat; background-size: auto 101%; }</style>`,
+      headStyle: `<style> .head_box { background: url(${this._path}/plugins/genshin/resources/img/namecard/${headImg}) #fff; background-position-x: 42px; background-repeat: no-repeat; background-size: auto 101%; }</style>`,
       srtempFile: ''
     }
   }
