3f116787055da68c9d32ff239e18c3c2c8b8c06b
柠檬冲水 <10424920+ningmengchongshui@user.noreply.gitee.com>
2023-09-03 10:24:26 +0000

    解决部分用户无法安装sqlite3,pup等安装问题,
    Signed-off-by: 柠檬冲水 <10424920+ningmengchongshui@user.noreply.gitee.com>

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,27 +1,84 @@
 const os = require("os");
-const { existsSync } = require("fs");
+const { existsSync, realpathSync } = require("fs");
+const { execSync } = require("child_process");
 const arch = os.arch();
+/**
+ * Downloa
+ */
 let skipDownload = false;
+/**
+ * Path
+ */
 let executablePath;
-
-// win32 存在 Edge 优先选择
-if (process.platform == "win32") {
-  if (existsSync("C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe")) {
-    skipDownload = true;
-    executablePath = "C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe";
+/**
+ * windows path
+ */
+const win32Edge =
+  "C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe";
+/**
+ * linux | android
+ */
+if (process.platform == "linux" || process.platform == "android") {
+  const chromium = [
+    "whereis chrome-browser",
+    "whereis chrome",
+    "whereis chromium-browser",
+    "whereis chromium",
+    "whereis firefox",
+  ];
+  /**
+   * get path
+   */
+  for (const item of chromium) {
+    try {
+      const chromiumPath = execSync(item).toString().split(" ")[1]?.trim();
+      if (chromiumPath) {
+        skipDownload = true;
+        executablePath = realpathSync(chromiumPath);
+        console.info("[Chromium] start");
+        break;
+      }
+    } catch (error) {
+      console.error("Failed to get Chromium path:", error);
+      continue;
+    }
   }
-} else if (process.platform == "linux") {
-  // 如果 arm64 架构且存在 Chromium，跳过下载
-  if ((arch == "arm64" || arch == "aarch64") && existsSync("/usr/bin/chromium")) {
-    skipDownload = true;
-    executablePath = "/usr/bin/chromium";
-  } else if (existsSync("/usr/bin/chromium")) {
-    // 不论什么架构，如果存在 Chromium，跳过下载且配置路径
-    skipDownload = true;
-    executablePath = "/usr/bin/chromium";
+  /**
+   * not path
+   */
+  if (!skipDownload) {
+    /**
+     * search
+     */
+    const arr = [
+      "/usr/bin/chromium",
+      "/snap/bin/chromium",
+      "/usr/bin/chromium-browser",
+    ];
+    for (const item of arr) {
+      if (existsSync(item)) {
+        skipDownload = true;
+        executablePath = item;
+        console.info("[Chromium] start");
+        break;
+      }
+    }
   }
+  /**
+   * arm64/arrch64
+   */
+  if (arch == "arm64" || arch == "aarch64") {
+    console.info("[arm64/aarch64] system");
+    skipDownload = true;
+  }
+} else if (process.platform == "win32" && existsSync(win32Edge)) {
+  /**
+   * win32  Edge
+   */
+  skipDownload = true;
+  executablePath = win32Edge;
+  console.info("[Win32 Edge] start");
 }
-
 /**
  * @type {import("puppeteer").Configuration}
  */
