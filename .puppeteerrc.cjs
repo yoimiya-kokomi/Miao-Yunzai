9a7fd50c65b5b5a5f5f08906f5a4e0bd94d366e1
🌌 <Time.Rain.Star.Sky@Gmail.com>
2023-09-06 13:00:35 +0800

    优化 puppeteer

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,5 +1,5 @@
 const os = require("os")
-const { existsSync, realpathSync } = require("fs")
+const { existsSync } = require("fs")
 const { execSync } = require("child_process")
 const arch = os.arch()
 
@@ -11,21 +11,20 @@ if (process.platform == "linux" || process.platform == "android")
     "chromium",
     "chromium-browser",
     "chrome",
-    "chrome-browser",
   ]) try {
     const chromiumPath = execSync(`command -v ${item}`).toString().trim()
     if (chromiumPath && existsSync(chromiumPath)) {
-      executablePath = realpathSync(chromiumPath)
+      executablePath = chromiumPath
       break
     }
   } catch (err) {}
 
 if (!executablePath) for (const item of [
-  "C:/Program Files/Google/Chrome/Application/chrome.exe",
-  "C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe",
   "/usr/bin/chromium",
   "/usr/bin/chromium-browser",
-  "/snap/bin/chromium",
+  "/usr/bin/chrome",
+  "C:/Program Files/Google/Chrome/Application/chrome.exe",
+  "C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe",
 ]) if (existsSync(item)) {
   executablePath = item
   break
