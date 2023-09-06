52cf19d0db084154c53190e727b2ad829708e35a
Ayakura Yuki <YukiAyakura@outlook.com>
2023-09-06 10:51:06 +0800

    更新 .puppeteerrc.cjs 以支持定位 macOS 上的 Chrome 或 Microsoft Edge 浏览器

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,39 +1,62 @@
-const os = require("os")
-const { existsSync, realpathSync } = require("fs")
-const { execSync } = require("child_process")
+const os = require('os')
+const {existsSync, realpathSync} = require('fs')
+const {execSync} = require('child_process')
 const arch = os.arch()
 
 let skipDownload = false
 let executablePath
 
-if (process.platform == "linux" || process.platform == "android")
+// linux / android
+if (process.platform === 'linux' || process.platform === 'android') {
   for (const item of [
-    "chromium",
-    "chromium-browser",
-    "chrome",
-    "chrome-browser",
-  ]) try {
-    const chromiumPath = execSync(`command -v ${item}`).toString().trim()
-    if (chromiumPath && existsSync(chromiumPath)) {
-      executablePath = realpathSync(chromiumPath)
-      break
+    'chromium',
+    'chromium-browser',
+    'chrome',
+    'chrome-browser',
+  ]) {
+    try {
+      const chromiumPath = execSync(`command -v ${item}`).toString().trim()
+      if (chromiumPath && existsSync(chromiumPath)) {
+        executablePath = realpathSync(chromiumPath)
+        break
+      }
+    } catch (err) {
     }
-  } catch (err) {}
-
-if (!executablePath) for (const item of [
-  "C:/Program Files/Google/Chrome/Application/chrome.exe",
-  "C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe",
-  "/usr/bin/chromium",
-  "/usr/bin/chromium-browser",
-  "/snap/bin/chromium",
-]) if (existsSync(item)) {
-  executablePath = item
-  break
+  }
 }
 
-if (executablePath || arch == "arm64" || arch == "aarch64") {
-  (typeof logger == "object" ? logger : console).info(`[Chromium] ${executablePath}`)
+// macOS
+if (process.platform === 'darwin') {
+  for (const item of [
+    '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+    '/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge',
+  ]) {
+    if (existsSync(item)) {
+      executablePath = item
+      break
+    }
+  }
+}
+
+// windows 或其他
+if (!executablePath) {
+  for (const item of [
+    'C:/Program Files/Google/Chrome/Application/chrome.exe',
+    'C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe',
+    '/usr/bin/chromium',
+    '/usr/bin/chromium-browser',
+    '/snap/bin/chromium',
+  ]) {
+    if (existsSync(item)) {
+      executablePath = item
+      break
+    }
+  }
+}
+
+if (executablePath || arch === 'arm64' || arch === 'aarch64') {
+  (typeof logger == 'object' ? logger : console).info(`[Chromium] ${executablePath}`)
   skipDownload = true
 }
 
-module.exports = { skipDownload, executablePath }
+module.exports = {skipDownload, executablePath}
