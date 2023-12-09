0b52a8fb8602916584dd86a3235329ce473b7214
🌌 <Time.Rain.Star.Sky@Gmail.com>
2023-12-09 10:15:58 +0800

    细节优化

--- plugins/system/quit.js
+++ plugins/system/quit.js
@@ -1,36 +1,36 @@
-import cfg from '../../lib/config/config.js'
-
+import cfg from "../../lib/config/config.js"
 export class quit extends plugin {
-  constructor () {
+  constructor() {
     super({
-      name: 'notice',
-      dsc: '自动退群',
-      event: 'notice.group.increase'
+      name: "notice",
+      dsc: "自动退群",
+      event: "notice.group.increase"
     })
   }
 
-  async accept () {
-    if (this.e.user_id != this.e.self_id) return
+  async accept() {
+    if (this.e.user_id != this.e.self_id) return false
+    if (!this.e.group?.quit) return false
 
-    let other = cfg.other
-    if (other.autoQuit <= 0) return
+    const other = cfg.other
+    if (!other.autoQuit) return false
 
     /** 判断主人，主人邀请不退群 */
-    let gl = await this.e.group.getMemberMap()
-    for (let qq of cfg.masterQQ) {
-      if (gl.has(Number(qq) || String(qq))) {
-        logger.mark(`[主人拉群] ${this.e.group_id}`)
-        return
+    if (this.e.group.getMemberMap) {
+      const gl = await this.e.group.getMemberMap()
+      for (const qq of cfg.masterQQ) {
+        if (gl.has(Number(qq) || String(qq))) {
+          logger.mark(`[主人拉群] ${this.e.group_id}`)
+          return false
+        }
       }
     }
 
     /** 自动退群 */
     if (Array.from(gl).length <= other.autoQuit && !this.e.group.is_owner) {
-      await this.e.reply('禁止拉群，已自动退出')
+      await this.reply("禁止拉群，已自动退出")
       logger.mark(`[自动退群] ${this.e.group_id}`)
-      setTimeout(() => {
-        this.e.group.quit()
-      }, 2000)
+      this.e.group.quit()
     }
   }
 }
