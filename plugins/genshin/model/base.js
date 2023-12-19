e7ff696c6c09ba0564070b17953b9d2d162ef640
枫叶 <id_09261891@qq.com>
2023-12-20 02:23:08 +0800

    更新原神4.3上半卡池 (#338)
    * Update 301.yaml
    
    * Update 302.yaml
    
    * Update pool.yaml
    
    * Update base.js

--- plugins/genshin/model/base.js
+++ plugins/genshin/model/base.js
@@ -50,7 +50,7 @@ export default class base {
         game: 'sr',
       }
     }
-    let char = Character.get('赛诺', 'gs')
+    let char = Character.get('娜维娅', 'gs')
     return {
       ...data,
       tplFile: `./plugins/genshin/resources/html/${this.model}/${this.model}.html`,
