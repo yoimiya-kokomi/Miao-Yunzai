070fdfa61edebf20cb356b23f79a4af83d84fd3b
🌌 <Time.Rain.Star.Sky@Gmail.com>
2024-01-13 22:25:19 +0800

    优化 状态

--- plugins/system/status.js
+++ plugins/system/status.js
@@ -1,124 +1,162 @@
-import cfg from '../../lib/config/config.js'
-import moment from 'moment'
+import cfg from "../../lib/config/config.js"
+import moment from "moment"
 
 export class status extends plugin {
   constructor() {
     super({
-      name: '其他功能',
-      dsc: '#状态',
-      event: 'message',
+      name: "状态统计",
+      dsc: "#状态",
+      event: "message",
       rule: [
         {
-          reg: '^#状态$',
-          fnc: 'status'
+          reg: "^#(状态|统计)",
+          fnc: "status",
         }
       ]
     })
+
+    this.key = "Yz:count:"
   }
 
   async status() {
-    if (this.e.isMaster) return this.statusMaster()
-    if (!this.e.isGroup) return this.reply('请群聊查看')
-    return this.statusGroup()
-  }
-
-  async statusMaster() {
-    let runTime = moment().diff(moment.unix(this.e.bot.stat.start_time), 'seconds')
-    let Day = Math.floor(runTime / 3600 / 24)
-    let Hour = Math.floor((runTime / 3600) % 24)
-    let Min = Math.floor((runTime / 60) % 60)
-    if (Day > 0) {
-      runTime = `${Day}天${Hour}小时${Min}分钟`
-    } else {
+    if (!this.e.isMaster) {
+      return this.reply(await this.getCount({
+        "用户": this.e.user_id,
+        "群": this.e.group_id,
+      }))
+    }
+    let runTime = moment().diff(moment.unix(this.e.bot.stat.start_time), "seconds")
+    const Day = Math.floor(runTime / 3600 / 24)
+    const Hour = Math.floor((runTime / 3600) % 24)
+    const Min = Math.floor((runTime / 60) % 60)
+    if (Day == 0)
       runTime = `${Hour}小时${Min}分钟`
-    }
+    else
+      runTime = `${Day}天${Hour}小时${Min}分钟`
 
-    let format = (bytes) => {
-      return (bytes / 1024 / 1024).toFixed(2) + 'MB'
-    }
+    const msg =
+      `—— TRSS Yunzai v${cfg.package.version} ——\n`+
+      `运行时间：${runTime}\n`+
+      `内存使用：${(process.memoryUsage().rss/1024/1024).toFixed(2)}MB\n`+
+      `系统版本：${process.platform} ${process.arch} ${process.version}`+
+      await this.count()
 
-    let msg = '-------状态-------'
-    msg += `\n运行时间：${runTime}`
-    msg += `\n内存使用：${format(process.memoryUsage().rss)}`
-    msg += `\n当前版本：v${cfg.package.version}`
-    msg += '\n-------累计-------'
-    msg += await this.getCount()
-
-    await this.reply(msg)
+    return this.reply(msg)
   }
 
-  async statusGroup() {
-    let msg = '-------状态-------'
-    msg += await this.getCount(this.e.group_id)
-
-    await this.reply(msg)
+  count() {
+    const cmd = {
+      msg: this.e.msg.replace(/^#(状态|统计)/, "").trim().split(" ")
+    }
+    let key = ""
+    for (const i of cmd.msg) if (key) {
+      cmd[key] = i
+      key = ""
+    } else {
+      key = i
+    }
+    return this.getCount(cmd)
   }
 
-  async getCount(groupId = '') {
-    this.date = moment().format('MMDD')
-    this.month = Number(moment().month()) + 1
-
-    this.key = 'Yz:count:'
-
-    if (groupId) this.key += `group:${groupId}:`
-
-    this.msgKey = {
-      day: `${this.key}sendMsg:day:`,
-      month: `${this.key}sendMsg:month:`
+  async getCount(cmd) {
+    const date = []
+    if (cmd["日期"]) {
+      cmd["日期"] = cmd["日期"].replace(/[^\d]/g,"")
+      switch (cmd["日期"].length) {
+        case 8:
+          date.push([
+            cmd["日期"].slice(0, 4),
+            cmd["日期"].slice(4, 6),
+            cmd["日期"].slice(6, 8),
+          ])
+          break
+        case 4:
+          date.push([
+            moment().format("YYYY"),
+            cmd["日期"].slice(0, 2),
+            cmd["日期"].slice(2, 4),
+          ])
+          break
+        case 2:
+          date.push([
+            moment().format("YYYY"),
+            moment().format("MM"),
+            cmd["日期"],
+          ])
+          break
+        default:
+          this.reply(`日期格式错误：${cmd["日期"]}`)
+          return ""
+      }
+    } else {
+      const d = moment()
+      for (let i = 0; i < 3; i++) {
+        date.push(d.format("YYYY MM DD").split(" "))
+        d.add(-86400000)
+      }
+      date.push(
+        [d.format("YYYY"), d.format("MM")],
+        [d.format("YYYY")],
+        ["total"],
+      )
     }
 
-    this.screenshotKey = {
-      day: `${this.key}screenshot:day:`,
-      month: `${this.key}screenshot:month:`
+    let msg = "消息统计"
+    if (cmd["消息"]) {
+      msg = `${cmd["消息"]} ${msg}`
+    } else {
+      cmd["消息"] = "msg"
     }
 
-    let week = {
-      msg: 0,
-      screenshot: 0
+    const array = []
+    if (cmd["机器人"])
+      array.push({ text: "机器人", key: `bot`, id: cmd["机器人"] })
+    if (cmd["用户"])
+      array.push({ text: "用户", key: `user`, id: cmd["用户"] })
+    if (cmd["群"])
+      array.push({ text: "群", key: `group`, id: cmd["群"] })
+    if (!array.length) {
+      array.push({ text: msg, key: "total" })
+      msg = ""
+      array.push({ type: "keys", text: "用户量", key: "user:*" })
+      array.push({ type: "keys", text: "群量", key: "group:*" })
+      if (this.e.self_id)
+        array.push({ text: "机器人", key: `bot`, id: this.e.self_id })
+      if (this.e.user_id)
+        array.push({ text: "用户", key: `user`, id: this.e.user_id })
+      if (this.e.group_id)
+        array.push({ text: "群", key: `group`, id: this.e.group_id })
     }
-    for (let i = 0; i <= 6; i++) {
-      let date = moment().startOf('week').add(i, 'days').format('MMDD')
 
-      week.msg += Number(await redis.get(`${this.msgKey.day}${date}`)) ?? 0
-      week.screenshot += Number(await redis.get(`${this.screenshotKey.day}${date}`)) ?? 0
-    }
-
-    let count = {
-      total: {
-        msg: await redis.get(`${this.key}sendMsg:total`) || 0,
-        screenshot: await redis.get(`${this.key}screenshot:total`) || 0
-      },
-      today: {
-        msg: await redis.get(`${this.msgKey.day}${this.date}`) || 0,
-        screenshot: await redis.get(`${this.screenshotKey.day}${this.date}`) || 0
-      },
-      week,
-      month: {
-        msg: await redis.get(`${this.msgKey.month}${this.month}`) || 0,
-        screenshot: await redis.get(`${this.screenshotKey.month}${this.month}`) || 0
+    for (const i of array) {
+      if (i.id) {
+        i.text += ` ${i.id}`
+        i.key += `:${i.id}`
+      }
+      msg += `\n\n${i.text}`
+      for (let d of date) {
+        const key = `:${cmd["消息"]}:${i.key}:${d.join(":")}`
+        d = d.join("-")
+        if (d == "total")
+          d = `总计 -------`
+        else
+          d = `${d} ${"-".repeat(11 - d.length)}`
+        const ret = await this.redis(i.type, key)
+        msg += `\n${d} 收 ${ret.receive} 发 ${ret.send}`
       }
     }
-
-    let msg = ''
-    if (groupId) {
-      msg = `\n发送消息：${count.today.msg}条`
-      msg += `\n生成图片：${count.today.screenshot}次`
-    } else {
-      msg = `\n发送消息：${count.total.msg}条`
-      msg += `\n生成图片：${count.total.screenshot}次`
-    }
-
-    if (count.month.msg > 200) {
-      msg += '\n-------本周-------'
-      msg += `\n发送消息：${count.week.msg}条`
-      msg += `\n生成图片：${count.week.screenshot}次`
-    }
-    if (moment().format('D') >= 8 && count.month.msg > 400) {
-      msg += '\n-------本月-------'
-      msg += `\n发送消息：${count.month.msg}条`
-      msg += `\n生成图片：${count.month.screenshot}次`
-    }
-
     return msg
+  }
+
+  async redis(type, key) {
+    const ret = {}
+    for (const i of ["receive", "send"]) {
+      const k = `${this.key}${i}${key}`
+      if (type == "keys")
+        ret[i] = (await redis.keys(k)).length || 0
+      else
+        ret[i] = await redis.get(k) || 0
+    }
+    return ret
   }
 }
