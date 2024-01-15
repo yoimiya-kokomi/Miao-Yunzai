6d8a74e6efc7de18f9bc8c2966909ba491f801ee
Rrrrrrray <28804884+Rrrrrrray@users.noreply.github.com>
2024-01-15 18:21:19 +0800

    #绑定uid适配3*

--- plugins/genshin/apps/user.js
+++ plugins/genshin/apps/user.js
@@ -95,7 +95,7 @@ export class user extends plugin {
   /** 绑定uid */
   saveUid () {
     if (!this.e.msg) return
-    let uid = this.e.msg.match(/[1|2|5-9][0-9]{8}/g)
+    let uid = this.e.msg.match(/[1|2|3|5-9][0-9]{8}/g)
     if (!uid) {
       this.reply('uid输入错误', false, { at: true })
       return
