d02490371f61caac09003e32d98af03e944e7351
🌌 <Time.Rain.Star.Sky@Gmail.com>
2024-04-29 09:52:48 +0800

    细节优化

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 switch (process.env.app_type || process.argv[2]) {
   case "pm2":
   case "start": {
-    global.Bot = new (await import("./lib/bot.js")).default
+    global.Bot = new Proxy(new (await import("./lib/bot.js")).default, {
+      get: (target, prop, receiver) => {
+        if (target[prop] !== undefined)
+          return target[prop]
+        for (const i of target.uin)
+          if (target[i]?.[prop] !== undefined) {
+            target.makeLog("trace", `因不存在 Bot.${prop} 而重定向到 Bot.${i}.${prop}`)
+            return target[i][prop]
+          }
+        target.makeLog("trace", `不存在 Bot.${prop}`)
+      }
+    })
     Bot.run()
     break
   } case "stop": {
