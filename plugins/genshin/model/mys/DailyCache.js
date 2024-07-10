0bf59f19a4ec5201df57441baf8b4538828100d1
kissnavel <xkn0722@gmail.com>
2024-07-10 19:08:43 +0800

    fix:更新zzz后国服和国际服互相调用缓存并判断ck失效

--- plugins/genshin/model/mys/DailyCache.js
+++ plugins/genshin/model/mys/DailyCache.js
@@ -40,7 +40,7 @@ export default class DailyCache extends BaseModel {
       key = 'sys:config'
     } else {
       game = MysUtil.getGameKey(game)
-      let serv = /^(1[0-9]|[6-9])|^hoyo|^os/i.test(uid) ? servs[1] : servs[0]
+      let serv = ['6', '7', '8', '9', '10', '13', '15', '17', '18'].includes(String(uid).slice(0, -8)) ? servs[1] : servs[0]
       key = `${game}:${serv}`
     }
     const date = moment().format('MM-DD')
