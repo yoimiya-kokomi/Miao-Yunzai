2861813de39cbadaec1cffbc85fdc3d58aa44cba
ZM.J <zgengen@qq.com>
2024-10-01 22:43:52 +0800

    Fix role combat: self ck should be used (#475)

--- plugins/genshin/model/mys/mysInfo.js
+++ plugins/genshin/model/mys/mysInfo.js
@@ -25,7 +25,7 @@ export default class MysInfo {
     }
     // ck对应MysUser对象
     this.ckUser = null
-    this.auth = ['dailyNote', 'bbs_sign_info', 'bbs_sign_home', 'bbs_sign', 'ys_ledger', 'compute', 'avatarSkill', 'detail', 'blueprint', 'UserGame', 'deckList', 'avatar_cardList', 'action_cardList', 'avatarInfo']
+    this.auth = ['dailyNote', 'bbs_sign_info', 'bbs_sign_home', 'bbs_sign', 'ys_ledger', 'compute', 'avatarSkill', 'detail', 'blueprint', 'UserGame', 'deckList', 'avatar_cardList', 'action_cardList', 'avatarInfo', 'role_combat']
 
     this.gtest = false
     this.mysButton = segment.button([
