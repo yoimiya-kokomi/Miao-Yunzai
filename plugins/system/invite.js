0b7547e847f2621f476d4abddbef994f21d4d738
micuks <sdkjwql@gmail.com>
2026-04-10 17:07:35 +0800

    feat: 添加 autoInvite 配置项，支持自动同意所有群邀请
    原先仅主人邀请时自动进群，其他人邀请仅记录日志。
    新增 autoInvite 配置项（other.yaml），设为 1 时所有好友邀请均自动同意进群，
    默认为 0 保持原有行为。

--- plugins/system/invite.js
+++ plugins/system/invite.js
@@ -1,18 +1,20 @@
+import cfg from "../../lib/config/config.js"
+
 export class invite extends plugin {
   constructor() {
     super({
       name: "invite",
-      dsc: "主人邀请自动进群",
+      dsc: "邀请自动进群",
       event: "request.group.invite",
     })
   }
 
   async accept() {
-    if (!this.e.isMaster) {
+    if (!this.e.isMaster && cfg.other.autoGroup !== 1) {
       logger.mark(`[邀请加群]：${this.e.group_name}：${this.e.group_id}`)
       return
     }
-    logger.mark(`[主人邀请加群]：${this.e.group_name}：${this.e.group_id}`)
+    logger.mark(`[${this.e.isMaster ? "主人" : "自动同意"}邀请加群]：${this.e.group_name}：${this.e.group_id}`)
     this.e.approve(true)
     this.e.bot.pickFriend(this.e.user_id).sendMsg(`已同意加群：${this.e.group_name}`)
   }
