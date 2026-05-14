0a10b1eab8178f5fa2283af9ec495ebc7655c8a2
Temmie <126144382+Temmie0125@users.noreply.github.com>
2026-05-14 20:19:19 +0800

    fix(adapter): 修复 OneBot v11 适配器全员禁言报错，增加戳一戳撤回事件支持
    - 修复全员禁言时`user_id=0`导致获取用户信息失败
    - 适配戳一戳撤回事件`poke_recall`

--- plugins/adapter/OneBotv11.js
+++ plugins/adapter/OneBotv11.js
@@ -1147,7 +1147,7 @@ Bot.adapter.push(
             `${data.self_id} <= ${data.group_id}`,
             true,
           )
-          data.bot.pickMember(data.group_id, data.user_id).getInfo()
+          if (data.user_id !== 0) data.bot.pickMember(data.group_id, data.user_id).getInfo()
           break
         case "group_msg_emoji_like":
           Bot.makeLog(
@@ -1179,6 +1179,22 @@ Bot.adapter.push(
                 Bot.makeLog(
                   "info",
                   `好友戳一戳：${data.operator_id} => ${data.target_id}`,
+                  data.self_id,
+                )
+              break
+            case "poke_recall":
+              data.operator_id = data.user_id
+              if (data.group_id)
+                Bot.makeLog(
+                  "info",
+                  `群戳一戳撤回：${data.operator_id} => ${data.target_id}`,
+                  `${data.self_id} <= ${data.group_id}`,
+                  true,
+                )
+              else
+                Bot.makeLog(
+                  "info",
+                  `好友戳一戳撤回：${data.operator_id} => ${data.target_id}`,
                   data.self_id,
                 )
               break
